7544aebd1544719e21aa9a2ec9c106f0613e2fe9
Foucsi <julien.foucartreact@gmail.com>
2022-09-20 16:06:59 +0200

    creation du context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
-const Application = styled.div``;
+const Application = styled.div`
+  height: 100vh;
+  width: 100%;
+`;
 
 function App() {
   return <Application></Application>;
