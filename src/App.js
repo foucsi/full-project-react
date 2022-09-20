383a99b60b4172f925a638e64376879b735746be
Foucsi <julien.foucartreact@gmail.com>
2022-09-20 16:11:37 +0200

    creation du composant Navigation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { Route, Routes } from "react-router-dom";
 import styled from "styled-components";
+import { UserContext } from "../src/Context/UserContext";
+import Home from "./Pages/Home";
 
 const Application = styled.div`
   height: 100vh;
   width: 100%;
+  background-color: tomato;
 `;
 
 function App() {
-  return <Application></Application>;
+  return (
+    <UserContext.Provider value={{}}>
+      <Application>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Application>
+    </UserContext.Provider>
+  );
 }
 
 export default App;
---
+++ src/Components/Navigation.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+function Navigation() {
+  return <Container>Navigation</Container>;
+}
+
+export default Navigation;
